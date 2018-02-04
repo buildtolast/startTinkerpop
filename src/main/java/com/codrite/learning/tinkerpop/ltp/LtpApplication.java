164be2da0f0af7086c19e7387d38a98888fa62a3
builttolast <builttolast@codrite.com>
2018-02-04 09:26:40 -0500

    Initial commit

---
+++ src/main/java/com/codrite/learning/tinkerpop/ltp/LtpApplication.java
@@ -0,0 +1,12 @@
+package com.codrite.learning.tinkerpop.ltp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtpApplication.class, args);
+	}
+}
